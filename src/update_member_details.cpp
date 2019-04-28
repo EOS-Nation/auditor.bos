80f952447843433c4a0a169ea8ca5f613a04a50f
Khazius <nsrempel@gmail.com>
2019-04-28 23:10:14 +1000

    Erase bio with empty string

--- src/update_member_details.cpp
+++ src/update_member_details.cpp
@@ -4,14 +4,23 @@ void auditor::updatebio(name cand, string bio) {
     const auto &reg_candidate = registered_candidates.get(cand.value, "ERR::UPDATEBIO_NOT_CURRENT_REG_CANDIDATE::Candidate is not already registered.");
     
     auto cand_bio = candidate_bios.find(cand.value);
-    if (cand_bio != candidate_bios.end()) {
-        candidate_bios.modify(cand_bio, cand, [&](auto &b) {
-            b.bio = bio;
-        });
+
+    if(bio.size() > 0) {
+        if (cand_bio != candidate_bios.end()) {
+            candidate_bios.modify(cand_bio, cand, [&](auto &b) {
+                b.bio = bio;
+            });
+        } else {
+            candidate_bios.emplace(cand, [&](auto &b) {
+                b.candidate_name = cand;
+                b.bio = bio;
+            });
+        }
     } else {
-        candidate_bios.emplace(cand, [&](auto &b) {
-            b.candidate_name = cand;
-            b.bio = bio;
-        });
+        if (cand_bio != candidate_bios.end()) {
+            candidate_bios.erase(cand_bio);
+        }
     }
+
+    
 }
